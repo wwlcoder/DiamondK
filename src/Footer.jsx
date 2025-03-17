66d8a8971a0aef69c0c0d6a58fa1174d63abfc7f
Nigel Seah <sshao-ju@42-bkk.com>
2025-03-17 12:53:51 +0700

    Changed Footer Bar Size

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -7,8 +7,8 @@ const Footer = () => {
 			className="bg-rose-100 dark:bg-slate-950 text-slate-900 dark:text-slate-100 text-xl sticky bottom-0 z-50"
 		>
 			<section className="max-w-4xl mx-auto p-4 flex flex-col sm:flex-row justify-between sm:justify-between">
-				<div className="flex flex-col sm:gap-2 basis-1/3 justify-center">
-					<h2 className="font-bold text-2xl text-center">Contact ♦️ K Poker</h2>
+				<div className="flex flex-col text-xl sm:text-2xl sm:gap-2 basis-1/3 justify-center">
+					<h2 className="font-bold text-center">Contact ♦️ K Poker</h2>
 					<a href="tel:+855123456789" className="text-center">Phone: +855 1234 56789</a>
 
 				</div>
@@ -17,8 +17,8 @@ const Footer = () => {
 					<HashLink to='/#testimonials'>Testimonials</HashLink>
 					<HashLink to='/rules#handranks'>Rules</HashLink>
 				</nav>
-				<div className="flex flex-col sm:gap-2 basis-1/3 justify-center">
-					<h2 className="font-bold text-2xl text-center">Find us Here!</h2>
+				<div className="hidden md:flex flex-col sm:gap-2 text-base sm:text-2xl basis-1/3 justify-center">
+					<h2 className="font-bold text-center">Find us Here!</h2>
 					<p className="text-center">ZheShang Casino, Sihanoukville, KH 18000</p>
 				</div>
 			</section>
