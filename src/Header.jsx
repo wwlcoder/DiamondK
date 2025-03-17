da5c63b7dcc6d16ff1d9fccf678f557a9f37200c
Nigel Seah <sshao-ju@42-bkk.com>
2025-03-17 17:45:53 +0700

    Changed Lang Button Size

--- src/Header.jsx
+++ src/Header.jsx
@@ -44,8 +44,8 @@ const Header = () => {
 					<div className="relative text-left">
 						<div>
 							<button type="button" onClick={toggleDropdown} className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" id="options-menu-mobile" aria-expanded="true" aria-haspopup="true">
-								{i18n.language === 'en' ? 'English' : '中文'}
-								<svg className="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+								{i18n.language === 'en' ? 'EN' : '中文'}
+								<svg className="-mr-1 ml-2 h-4 w-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
 									<path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 011.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
 								</svg>
 							</button>
